972d8a18fab708cee65126f6ecabf9cc3fbb5844
ChengengLu <qq844303749@gmail.com>
2020-03-24 12:39:57 +0800

    Initial commit

---
+++ src/main/kotlin/xyz/luchengeng/callr/CallrApplication.kt
@@ -0,0 +1,11 @@
+package xyz.luchengeng.callr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CallrApplication
+
+fun main(args: Array<String>) {
+    runApplication<CallrApplication>(*args)
+}
---
+++ src/test/kotlin/xyz/luchengeng/callr/CallrApplicationTests.kt
@@ -0,0 +1,13 @@
+package xyz.luchengeng.callr
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CallrApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
